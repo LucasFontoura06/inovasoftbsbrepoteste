b7234462f531ec6e8e0902b849ebe47c3385327b
Lucas Fontoura <lucasfontoura@Lucass-MacBook-Air.local>
2024-11-21 13:49:42 -0300

    Adicinei o logo e mudei o nome da aba

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import { LanguageProvider } from './contexts/LanguageContext';
 import { BrowserRouter as Router} from "react-router-dom";
 import HeroSection from "./componentes/HeroSection";
-import AboutUs from "./componentes/AboutUs";
-import Products from "./componentes/Products";
-import Footer from "./componentes/Footer";
 import Navigation from "./componentes/Navigation";
-import { LanguageProvider } from './contexts/LanguageContext';
 import { GlobalStyle } from './style/GlobalStyle';
+import Products from "./componentes/Products";
+import AboutUs from "./componentes/AboutUs";
+import Footer from "./componentes/Footer";
 
 function App() {
   return (
